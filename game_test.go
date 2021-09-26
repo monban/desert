84ff403b8e56579b583d8f5fb860a508e86ae48e
Ian Flynn <himitsu.monban@gmail.com>
2021-09-25 21:53:33 -0400

    add tests for game

---
+++ game_test.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/matryer/is"
+)
+
+func TestDrawStormCard(t *testing.T) {
+	is := is.NewRelaxed(t)
+	g := NewGame()
+	ol := len(g.StormDeck.Cards)
+	c := g.DrawStormCard()
+	is.Equal(c, g.StormDiscard.Cards[0])
+	is.Equal(ol-1, len(g.StormDeck.Cards))
+}
+
+func TestDrawGearCard(t *testing.T) {
+	is := is.NewRelaxed(t)
+	g := NewGame()
+	ol := len(g.GearDeck.Cards)
+	c := g.DrawGearCard()
+	is.Equal(c, g.GearDiscard.Cards[0])
+	is.Equal(ol-1, len(g.GearDeck.Cards))
+}

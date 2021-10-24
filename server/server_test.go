977b51a35bef845fc5bc06ef08b5f6ddc778e0d3
Ian Flynn <himitsu.monban@gmail.com>
2021-10-23 23:52:37 -0400

    test some things

--- server/server_test.go
+++ server/server_test.go
@@ -3,6 +3,7 @@ package server
 import (
 	"context"
 	"testing"
+	"time"
 
 	"github.com/nats-io/nats-server/v2/test"
 	"github.com/nats-io/nats.go"
@@ -18,11 +19,32 @@ func RunServer(fn func(*nats.Conn)) {
 
 func TestSomething(t *testing.T) {
 	RunServer(func(nc *nats.Conn) {
-		ctx, done := context.WithCancel(context.Background())
+		wait := make(chan struct{})
+		type action struct {
+			Action string
+		}
+		ec, _ := nats.NewEncodedConn(nc, "json")
+		//is := is.New(t)
+		ctx, done := context.WithTimeout(context.Background(), time.Second*2)
 		defer done()
+		ec.Subscribe("GAMES.0.ACTION", func(a action) {
+			t.Logf("%+v", a)
+			wait <- struct{}{}
+		})
 		Run(ctx)
-		nc.RequestWithContext(ctx, "GAMES.NEW", []byte(""))
-		msg, _ := nc.RequestWithContext(ctx, "GAMES.0.DRAW.storm", []byte(""))
-		t.Log(msg)
+		newGameData := struct {
+			Name string
+			Pid  uint64
+		}{"New Game", 0}
+		var res interface{}
+		a := action{"drawstorm"}
+		ec.RequestWithContext(ctx, "GAMES.NEW", newGameData, res)
+		ec.Publish("GAMES.0.ACTION", a)
+		select {
+		case <-wait:
+			t.Log("the function was called")
+		case <-ctx.Done():
+			t.Fatal("function was not called")
+		}
 	})
 }
